9ddafe64b8bbbd89ccd76bef5bef3b7fa0fad8a3
Apple <apple@Apples-MacBook-Pro.local>
2020-01-05 15:39:28 +0700

    Check Accordion

--- application/controllers/api/v1/Dashboard.php
+++ application/controllers/api/v1/Dashboard.php
@@ -139,154 +139,73 @@ class Dashboard extends REST_Controller
                                 <li>Penanganan dan pemindahannya bersifat TERAKHIR</li>
                             </ul>",
                             "<img src='http://atp-sb-medis.com/image/informasi_5_1.jpg'/><br><br><img src='http://atp-sb-medis.com/image/informasi_5_2.jpg'/>",
-                            "
-                            <html>
+                            "<html lang='en'>
                             <head>
-                            <style>
-                                .accordion {
-                                    background-color: #eee;
-                                    color: #444;
-                                    cursor: pointer;
-                                    padding: 18px;
-                                    width: 100%;
-                                    border: none;
-                                    text-align: left;
-                                    outline: none;
-                                    font-size: 15px;
-                                    transition: 0.4s;
-                                }
-                                
-                                .active, .accordion:hover {
-                                    background-color: #ccc; 
-                                }
-
-                                .accordion:after {
-                                    content: '\02795';
-                                    font-size: 13px;
-                                    color: #777;
-                                    float: right;
-                                    margin-left: 5px;
-                                }
-                                  
-                                .active:after {
-                                    content: '\2796';
-                                }
-                                
-                                .panel {
-                                    padding: 0 18px;
-                                    display: none;
-                                    background-color: white;
-                                    overflow: hidden;
-                                }
-                            </style>
+                                <meta charset='utf-8'>
+                                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                                <link
+                                    rel='stylesheet'
+                                    href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+                                <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>
+                                <script
+                                    src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
+                                <style type='text/css'>
+                                    .bs-example {
+                                        margin: 20px;
+                                    }
+                                </style>
                             </head>
                             <body>
-                            <button class='accordion'>Pemeriksaan Primer</button>
-                            <div class='panel'>
-                                <p>
-                                    Pemeriksaan primer adalah deteksi cepat dan koreksi segera terhadap kondisi yang mengancam jiwa.
-                                    <b align='center'>Cara Pelaksanaannya :</b>
-                                    <ul>
-                                        <li>
-                                            Jalan Nafas <br>
-                                            Lihat, dengar Raba <br>
-                                            Atasi segera, bebaskan jalan nafas <br>
-                                            Penghisapan
-                                        </li>
-                                        <li>
-                                            Pernafasan <br>
-                                            Apakah pertukaran hawa panas adekuat? <br>
-                                            a. Tidak ada, lakukan bantuan nafas <br>
-                                            b. Frekuansi <br>
-                                            c. Kualitas <br>
-                                            d. Teratur <br>
-                                            e. Ujung kuku biru atau tidak
-                                        </li>
-                                        <li>Perdarahan <br>
-                                            - Diluar : <br>
-                                            Hentikan segera : <br>
-                                            <ul>
-                                                <li>Dengan bebat tekan pada luka</li>
-                                                <li>Bagian luka yang ditinggikan</li>
-                                                <li>Kompress es</li>
-                                                <li>Tourniquet (hanya pada luka khusus)</li>
-                                            </ul>
-                                            <br>
-                                            - Didalam :
-                                            <ul>
-                                                <li>Kirim segera</li>
-                                            </ul>
-                                        </li>
-                                        <li>
-                                            Tulang Belakang <br>
-                                            Apakah sadar? <br>
-                                            Adakah trauma kepala? <br>
-                                            Stabilisasi leher dan tulang belakang <br>
-                                            Sebelum dikirim <br>
-                                        </li>
-                                        <li>
-                                            Shock <br>
-                                            Tanda-tanda shock <br>
-                                            Stabilitas segera kirim <br>
-                                        </li>
-                                    </ul>
-                                </p>
-                            </div>
-                    
-                            <button class='accordion'>Pemeriksaan Sekunder</button>
-                            <div class='panel'>
-                                <p>
-                                    Pemeriksaan sekunder adalah mencari perubahan-perubahan yang dapat berkembang menjadi lebih
-                                    gawat dan dapat mengancamjiwa apabila tidak segera diatasi.
-
-                                    <b align='center'>Cara Pelaksanaannya :</b>
-                                    <ul>
-                                        <li>
-                                            Periksa kondisi menyeluruh
-                                            1. Posisi saat ditemukan
-                                            2. Tingkat kesadaran
-                                            3. Sikap umum dan keluhan
-                                            4. Ruda paksa, kelainan
-                                            5. Keadaan sulit
-                                        </li>
-                                        <li>
-                                            Periksa kepala dan leher <br>
-                                            Rambut, kulit kepala, telinga, mata, hidung, mulut, ada kejang otot leher/tidak
-                                        </li>
-                                        <li>
-                                            Periksa dada dan perut <br>
-                                            Ada luka di dada, kelainan bentuk, perut tegang
-                                        </li>
-                                        <li>
-                                            Periksa anggota gerak atas dan bawah <br>
-                                            Luka, bengkak, nyeri untuk gerak
-                                        </li>
-                                    </ul>
-                                </p>
-                            </div>
-                    
-                            <script>
-                                var acc = document.getElementsByClassName('accordion');
-                                var i;
-                    
-                                for (i = 0; i < acc.length; i++) {
-                                    if (i == 0)
-                                        acc[i].click();
-                                        
-                                    acc[i].addEventListener('click', function () {
-                                        this
-                                            .classList
-                                            .toggle('active');
-                                        var panel = this.nextElementSibling;
-                                        if (panel.style.display === 'block') {
-                                            panel.style.display = 'none';
-                                        } else {
-                                            panel.style.display = 'block';
-                                        }
-                                    });
-                                }
-                            </script>
-                        </body>
+                                <div class='bs-example'>
+                                    <div class='panel-group' id='accordion'>
+                                        <div class='panel panel-default'>
+                                            <div class='panel-heading'>
+                                                <a
+                                                    style='text-decoration:none'
+                                                    data-toggle='collapse'
+                                                    data-parent='#accordion'
+                                                    href='#collapseOne'>
+                                                    <h4 class='panel-title'>
+                                                        1. What is HTML?
+                                                    </h4>
+                                                </a>
+                                            </div>
+                                            <div id='collapseOne' class='panel-collapse collapse in'>
+                                                <div class='panel-body'>
+                                                    <p>HTML stands for HyperText Markup Language. HTML is the standard markup
+                                                        language for describing the structure of web pages.
+                                                        <a href='https://www.tutorialrepublic.com/html-tutorial/' target='_blank'>Learn more.</a>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class='panel panel-default'>
+                                            <div class='panel-heading'>
+                                                <a
+                                                    style='text-decoration:none'
+                                                    data-toggle='collapse'
+                                                    data-parent='#accordion'
+                                                    href='#collapseTwo'>
+                                                    <h4 class='panel-title'>
+                                                        1. What is HTML?
+                                                    </h4>
+                                                </a>
+                                            </div>
+                                            <div id='collapseTwo' class='panel-collapse collapse'>
+                                                <div class='panel-body'>
+                                                    <p>Bootstrap is a sleek, intuitive, and powerful front-end framework for faster
+                                                        and easier web development. It is a collection of CSS and HTML conventions.
+                                                        <a
+                                                            href='https://www.tutorialrepublic.com/twitter-bootstrap-tutorial/'
+                                                            target='_blank'>Learn more.</a>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </body>
                         </html>"
                         );
         $tempData       = array();
