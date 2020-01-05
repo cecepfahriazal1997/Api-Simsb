53caa4c640419279391de1acea4b5554bd804e08
Apple <apple@Apples-MacBook-Pro.local>
2020-01-05 15:10:50 +0700

    Update disaster

--- application/controllers/api/v1/Dashboard.php
+++ application/controllers/api/v1/Dashboard.php
@@ -139,50 +139,33 @@ class Dashboard extends REST_Controller
                                 <li>Penanganan dan pemindahannya bersifat TERAKHIR</li>
                             </ul>",
                             "<img src='http://atp-sb-medis.com/image/informasi_5_1.jpg'/><br><br><img src='http://atp-sb-medis.com/image/informasi_5_2.jpg'/>",
-                            "
-                            <html>
-                            <head>
-                            <style>
+                            "<style>
                                 .accordion {
-                                background-color: #eee;
-                                color: #444;
-                                cursor: pointer;
-                                padding: 18px;
-                                width: 100%;
-                                border: none;
-                                text-align: left;
-                                outline: none;
-                                font-size: 15px;
-                                transition: 0.4s;
+                                    background-color: #eee;
+                                    color: #444;
+                                    cursor: pointer;
+                                    padding: 18px;
+                                    width: 100%;
+                                    border: none;
+                                    text-align: left;
+                                    outline: none;
+                                    font-size: 15px;
+                                    transition: 0.4s;
                                 }
                                 
                                 .active, .accordion:hover {
                                     background-color: #ccc; 
                                 }
-
-                                .accordion:after {
-                                    content: '\002B';
-                                    color: #777;
-                                    font-weight: bold;
-                                    float: right;
-                                    margin-left: 5px;
-                                }
-
-                                .active:after {
-                                    content: '\002D';
-                                }
                                 
                                 .panel {
                                     padding: 0 18px;
+                                    display: none;
                                     background-color: white;
-                                    max-height: 0;
                                     overflow: hidden;
-                                    transition: max-height 0.2s ease-out;
                                 }
                             </style>
-                            </head>
-                            <body>
-                            <button class='accordion active'>Pemeriksaan Primer</button>
+
+                            <button class='accordion'>Pemeriksaan Primer</button>
                             <div class='panel'>
                                 <p>
                                     Pemeriksaan primer adalah deteksi cepat dan koreksi segera terhadap kondisi yang mengancam jiwa.
@@ -283,9 +266,7 @@ class Dashboard extends REST_Controller
                                         }
                                     });
                                 }
-                            </script>
-                        </body>
-                        </html>"
+                            </script>"
                         );
         $tempData       = array();
 
