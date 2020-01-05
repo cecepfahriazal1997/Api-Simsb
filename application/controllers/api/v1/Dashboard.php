bd41dd1be157d315deaa36dd3c6f649cbd98494f
Apple <apple@Apples-MacBook-Pro.local>
2020-01-05 14:29:15 +0700

    Update disaster

--- application/controllers/api/v1/Dashboard.php
+++ application/controllers/api/v1/Dashboard.php
@@ -103,10 +103,121 @@ class Dashboard extends REST_Controller
     }
 
     public function prepareDisaster_get() {
-        $title          = array('Saat Bencana 1', 'Saat Bencana 2');
+        $title          = array(
+                            'Saat Bencana',
+                            'Sistem Rujukan',
+                            'Klasifikasi Bencana',
+                            'Pasien Triase',
+                            'Pasien Prioritas',
+                            'Pasien Transportasi',
+                            'Pasien Terapi',
+                        );
         $image          = array(
-                            "<img src='http://atp-sb-medis.com/image/bencana_1.jpg'/>",
-                            "<img src='http://atp-sb-medis.com/image/bencana_1.jpg'/>"
+                            "<img src='http://atp-sb-medis.com/image/informasi_1.jpg'/>",
+                            "<img src='http://atp-sb-medis.com/image/informasi_2.jpg'/>",
+                            "<img src='http://atp-sb-medis.com/image/informasi_3.jpg'/>",
+                            "<img src='http://atp-sb-medis.com/image/informasi_4.jpg'/>",
+                            "<h4 align='center'>Prioritas Pertama</h4>
+                            <ul>
+                                <li>Mengancam jiwa/mengancam fungsi vital.</li>
+                                <li>Penanganan dan pemindahan bersifat SEGERA</li>
+                            </ul>
+                            <h4 align='center'>Prioritas Kedua</h4>
+                            <ul>
+                                <li>Potensial mengancam jiwa/mengancam fungsi vital, bila tidak segera ditangani
+                                    dalam waktu
+                                </li>
+                            </ul>
+                            <h4 align='center'>Singkat</h4>
+                            <ul>
+                                <li>Penanganan dan pemindahan bersifat JANGAN TERLAMBAT</li>
+                            </ul>
+                            <h4 align='center'>Prioritas Ketiga</h4>
+                            <ul>
+                                <li>Perlu penanganannya seperti pelayanan biasa</li>
+                                <li>Tidak perlu segera</li>
+                                <li>Penanganan dan pemindahannya bersifat TERAKHIR</li>
+                            </ul>",
+                            "<img src='http://atp-sb-medis.com/image/informasi_5_1.jpg'/><br><img src='http://atp-sb-medis.com/image/informasi_5_1.jpg'/>",
+                            "<html>
+                            <head>
+                                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                                <style>
+                                    .accordion {
+                                        background-color: #eee;
+                                        color: #444;
+                                        cursor: pointer;
+                                        padding: 18px;
+                                        width: 100%;
+                                        border: none;
+                                        text-align: left;
+                                        outline: none;
+                                        font-size: 15px;
+                                        transition: 0.4s;
+                                    }
+                        
+                                    .accordion:hover,
+                                    .active {
+                                        background-color: #ccc;
+                                    }
+                        
+                                    .panel {
+                                        padding: 0 18px;
+                                        display: none;
+                                        background-color: white;
+                                        overflow: hidden;
+                                    }
+                                </style>
+                            </head>
+                            <body>
+                        
+                                <h2>Accordion</h2>
+                        
+                                <button class='accordion'>Section 1</button>
+                                <div class='panel'>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat.</p>
+                                </div>
+                        
+                                <button class='accordion'>Section 2</button>
+                                <div class='panel'>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat.</p>
+                                </div>
+                        
+                                <button class='accordion'>Section 3</button>
+                                <div class='panel'>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat.</p>
+                                </div>
+                        
+                                <script>
+                                    var acc = document.getElementsByClassName('accordion');
+                                    var i;
+                        
+                                    for (i = 0; i < acc.length; i++) {
+                                        acc[i].addEventListener('click', function () {
+                                            this
+                                                .classList
+                                                .toggle('active');
+                                            var panel = this.nextElementSibling;
+                                            if (panel.style.display === 'block') {
+                                                panel.style.display = 'none';
+                                            } else {
+                                                panel.style.display = 'block';
+                                            }
+                                        });
+                                    }
+                                </script>
+                        
+                            </body>
+                        </html>"
                         );
         $tempData       = array();
 
