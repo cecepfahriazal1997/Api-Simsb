38187d1b2c1da233bc7522192c2ba18e238d6613
Cecep Rokani <cecepfahriazal1997@gmail.com>
2020-04-25 01:14:22 +0700

    Add meta tag

--- application/views/natural_disaster.php
+++ application/views/natural_disaster.php
@@ -1,6 +1,7 @@
 <html>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'>
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>
     <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'></script>
